c6d661bfa150be24e2b392b053b3c3fcc2b2688c
bshubham01 <bshubham7447@gmail.com>
2024-09-27 00:15:59 +0530

    test cases passed

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+from library.library import Book, Library
+
+# tests/test_library.py
 import pytest
 from library.library import Book, Library
 
