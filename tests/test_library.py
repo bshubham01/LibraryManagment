66a76619755a05f320a1a576171de1e153fab16e
bshubham01 <bshubham7447@gmail.com>
2024-09-26 23:39:32 +0530

    corrected the errors

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+from library.library import Book, Library
+
+
 import pytest
 from library.library import Book, Library
 
