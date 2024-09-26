89f074364559d6fa98765e6db9068638037d8dcb
bshubham01 <bshubham7447@gmail.com>
2024-09-26 23:45:08 +0530

    tested for avilable books

--- tests/test_library.py
+++ tests/test_library.py
@@ -35,4 +35,18 @@ def test_return_book():
 
 
 
+# Test for viewing available books
+def test_available_books():
+    library = Library()
+    book1 = Book("12345", "Test Book", "Author", 2020)
+    book2 = Book("67890", "Another Book", "Author", 2019)
+    library.add_book(book1)
+    library.add_book(book2)
+    library.borrow_book("12345")
+    available_books = library.available_books()
+    assert len(available_books) == 1
+    assert available_books[0].isbn == "67890"
+
+
+
 
